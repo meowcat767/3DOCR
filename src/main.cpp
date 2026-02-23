6d113a9fdaf8e4dfb77974159f59508717caafe6
Ben <meowcat@meowcat.site>
2026-02-23 17:16:14 +0000

    extract takepicure3d function to a new file

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,7 @@
 #include <3ds.h>
 #include <stdio.h>
 
-#define WIDTH 400
-#define HEIGHT 240
-#define SCREEN_SIZE WIDTH * HEIGHT * 2
-#define BUF_SIZE SCREEN_SIZE * 2
 
-#define CONFIG_3D_SLIDERSTATE (*(volatile float*)0x1FF81080)
-#define WAIT_TIMEOUT 300000000ULL
-
-void takePicture3D(u8 *buf) {
-    u32 bufSize;
-    printf("CAMU_GetMaxBytes: 0x%08X\n", (unsigned int) CAMU_GetMaxBytes(&bufSize, WIDTH, HEIGHT));
-    printf("CAMU_SetTransferBytes: 0x%08X\n", (unsigned int) CAMU_SetTransferBytes(PORT_BOTH, bufSize, WIDTH, HEIGHT));
-
-    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_OUT1_OUT2));
-
-    Handle camReceiveEvent = 0;
-    Handle camReceiveEvent2 = 0;
-
-    printf("CAMU_ClearBuffer: 0x%08X\n", (unsigned int) CAMU_ClearBuffer(PORT_BOTH));
-    printf("CAMU_SynchronizeVsyncTiming: 0x%08X\n", (unsigned int) CAMU_SynchronizeVsyncTiming(SELECT_OUT1, SELECT_OUT2));
-
-    printf("CAMU_StartCapture: 0x%08X\n", (unsigned int) CAMU_StartCapture(PORT_BOTH));
-
-    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent, buf, PORT_CAM1, SCREEN_SIZE, (s16) bufSize));
-    printf("CAMU_SetReceiving: 0x%08X\n", (unsigned int) CAMU_SetReceiving(&camReceiveEvent2, buf + SCREEN_SIZE, PORT_CAM2, SCREEN_SIZE, (s16) bufSize));
-    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent, WAIT_TIMEOUT));
-    printf("svcWaitSynchronization: 0x%08X\n", (unsigned int) svcWaitSynchronization(camReceiveEvent2, WAIT_TIMEOUT));
-    printf("CAMU_PlayShutterSound: 0x%08X\n", (unsigned int) CAMU_PlayShutterSound(SHUTTER_SOUND_TYPE_NORMAL));
-
-    printf("CAMU_StopCapture: 0x%08X\n", (unsigned int) CAMU_StopCapture(PORT_BOTH));
-
-    svcCloseHandle(camReceiveEvent);
-    svcCloseHandle(camReceiveEvent2);
-
-    printf("CAMU_Activate: 0x%08X\n", (unsigned int) CAMU_Activate(SELECT_NONE));
-}
 
 int main() {
     gfxInitDefault();
